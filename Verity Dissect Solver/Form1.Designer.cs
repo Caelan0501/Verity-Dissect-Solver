3e8e0fae650d79525190ae3199f07db22d828cd6
Caelan Desmond <ccdesmon@sundevils.asu.edu>
2024-08-30 02:16:05 -0700

    Add project files.

---
+++ Verity Dissect Solver/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace Verity_Dissect_Solver
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.ResultLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Sphere",
+            "Prism",
+            "Cube",
+            "Cylinder",
+            "Pyramid",
+            "Cone"});
+            this.comboBox1.Location = new System.Drawing.Point(74, 242);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Sphere",
+            "Prism",
+            "Cube",
+            "Cylinder",
+            "Pyramid",
+            "Cone"});
+            this.comboBox2.Location = new System.Drawing.Point(367, 242);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 1;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "Sphere",
+            "Prism",
+            "Cube",
+            "Cylinder",
+            "Pyramid",
+            "Cone"});
+            this.comboBox3.Location = new System.Drawing.Point(629, 242);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 21);
+            this.comboBox3.TabIndex = 2;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(376, 58);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // ResultLabel
+            // 
+            this.ResultLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ResultLabel.Location = new System.Drawing.Point(69, 439);
+            this.ResultLabel.Name = "ResultLabel";
+            this.ResultLabel.Size = new System.Drawing.Size(753, 335);
+            this.ResultLabel.TabIndex = 10;
+            this.ResultLabel.Text = "Result appears here";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(389, 413);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Solve";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::Verity_Dissect_Solver.Properties.Resources.prism;
+            this.pictureBox6.Location = new System.Drawing.Point(629, 269);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(121, 121);
+            this.pictureBox6.TabIndex = 8;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::Verity_Dissect_Solver.Properties.Resources.cone;
+            this.pictureBox5.Location = new System.Drawing.Point(367, 269);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(121, 121);
+            this.pictureBox5.TabIndex = 7;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::Verity_Dissect_Solver.Properties.Resources.cylinder;
+            this.pictureBox4.Location = new System.Drawing.Point(74, 269);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(121, 121);
+            this.pictureBox4.TabIndex = 6;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(629, 115);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(121, 121);
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(367, 115);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(121, 121);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Verity_Dissect_Solver.Properties.Resources.sphere;
+            this.pictureBox1.Location = new System.Drawing.Point(74, 115);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(121, 121);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(929, 801);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ResultLabel);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label ResultLabel;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Verity Dissect Solver/Form1.cs
@@ -0,0 +1,445 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Verity_Dissect_Solver
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox2.Text == "Sphere")
+            {
+                pictureBox2.Image = Properties.Resources.sphere;
+            }
+            else if (comboBox2.Text == "Prism")
+            {
+                pictureBox2.Image = Properties.Resources.prism;
+            }
+            else if (comboBox2.Text == "Cube")
+            {
+                pictureBox2.Image = Properties.Resources.cube;
+            }
+            else if ((comboBox2.Text == "Cone"))
+            {
+                pictureBox2.Image = Properties.Resources.cone;
+            }
+            else if ((comboBox2.Text == "Cylinder"))
+            {
+                pictureBox2.Image = Properties.Resources.cylinder;
+            }
+            else
+            {
+                pictureBox2.Image = Properties.Resources.tetra;
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "Sphere")
+            {
+                pictureBox1.Image = Properties.Resources.sphere;
+            }
+            else if (comboBox1.Text == "Prism")
+            {
+                pictureBox1.Image = Properties.Resources.prism;
+            }
+            else if(comboBox1.Text == "Cube")
+            {
+                pictureBox1.Image = Properties.Resources.cube;
+            }
+            else if ((comboBox1.Text == "Cone"))
+            {
+                pictureBox1.Image = Properties.Resources.cone;
+            }
+            else if ((comboBox1.Text == "Cylinder"))
+            {
+                pictureBox1.Image = Properties.Resources.cylinder;
+            }
+            else
+            {
+                pictureBox1.Image = Properties.Resources.tetra;
+            }
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox3.Text == "Sphere")
+            {
+                pictureBox3.Image = Properties.Resources.sphere;
+            }
+            else if (comboBox1.Text == "Prism")
+            {
+                pictureBox3.Image = Properties.Resources.prism;
+            }
+            else if (comboBox3.Text == "Cube")
+            {
+                pictureBox3.Image = Properties.Resources.cube;
+            }
+            else if ((comboBox3.Text == "Cone"))
+            {
+                pictureBox3.Image = Properties.Resources.cone;
+            }
+            else if ((comboBox3.Text == "Cylinder"))
+            {
+                pictureBox3.Image = Properties.Resources.cylinder;
+            }
+            else
+            {
+                pictureBox3.Image = Properties.Resources.tetra;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if ((textBox1.Text.Length == 3))
+            {
+                if (textBox1.Text[0] == 'S' || textBox1.Text[0] == 's')
+                {
+                    pictureBox4.Image = Properties.Resources.cone;
+                }
+                else if (textBox1.Text[0] == 'C' || textBox1.Text[0] == 'c')
+                {
+                    pictureBox4.Image = Properties.Resources.prism;
+                }
+                else if (textBox1.Text[0] == 'T' || textBox1.Text[0] == 't')
+                {
+                    pictureBox4.Image = Properties.Resources.cylinder ;
+                }
+
+                if (textBox1.Text[1] == 'S' || textBox1.Text[1] == 's')
+                {
+                    pictureBox5.Image = Properties.Resources.cone;
+                }
+                else if (textBox1.Text[1] == 'C' || textBox1.Text[1] == 'c')
+                {
+                    pictureBox5.Image = Properties.Resources.prism;
+                }
+                else if (textBox1.Text[1] == 'T' || textBox1.Text[1] == 't')
+                {
+                    pictureBox5.Image = Properties.Resources.cylinder;
+                }
+
+                if (textBox1.Text[2] == 'S' || textBox1.Text[2] == 's')
+                {
+                    pictureBox6.Image = Properties.Resources.cone;
+                }
+                else if (textBox1.Text[2] == 'C' || textBox1.Text[2] == 'c')
+                {
+                    pictureBox6.Image = Properties.Resources.prism;
+                }
+                else if (textBox1.Text[2] == 'T' || textBox1.Text[2] == 't')
+                {
+                    pictureBox6.Image = Properties.Resources.cylinder;
+                }
+            }
+        }
+
+        private void Solve()
+        {
+            if (textBox1.Text.Length != 3)
+            {
+                return;
+            }
+            char AntiShape1 = textBox1.Text[0];
+            char AntiShape2 = textBox1.Text[1];
+            char AntiShape3 = textBox1.Text[2];
+
+            string one = comboBox1.Text;
+            string two = comboBox2.Text;
+            string three = comboBox3.Text;
+
+            char[] Shape1 = new char[2];
+            char[] Shape2 = new char[2];
+            char[] Shape3 = new char[2];
+            switch (one)
+            {
+                case "Cone":
+                {
+                    Shape1[0] = 'T';
+                    Shape1[1] = 'C';
+                    break;
+                }
+                case "Cube":
+                {
+                    Shape1[0] = 'S';
+                    Shape1[1] = 'S';
+                    break;
+                }
+                case "Sphere":
+                {
+                    Shape1[0] = 'C';
+                    Shape1[1] = 'C';
+                    break;
+                }
+                case "Prism":
+                {
+                    Shape1[0] = 'S';
+                    Shape1[1] = 'T';
+                    break;
+                }
+                case "Cylinder":
+                {
+                    Shape1[0] = 'C';
+                    Shape1[1] = 'S';
+                    break;
+                }
+                case "Pyramid":
+                {
+                    Shape1[0] = 'T';
+                    Shape1[1] = 'T';
+                    break;
+                }
+            }
+            switch (two)
+            {
+                case "Cone":
+                    {
+                        Shape2[0] = 'T';
+                        Shape2[1] = 'C';
+                        break;
+                    }
+                case "Cube":
+                    {
+                        Shape2[0] = 'S';
+                        Shape2[1] = 'S';
+                        break;
+                    }
+                case "Sphere":
+                    {
+                        Shape2[0] = 'C';
+                        Shape2[1] = 'C';
+                        break;
+                    }
+                case "Prism":
+                    {
+                        Shape2[0] = 'S';
+                        Shape2[1] = 'T';
+                        break;
+                    }
+                case "Cylinder":
+                    {
+                        Shape2[0] = 'C';
+                        Shape2[1] = 'S';
+                        break;
+                    }
+                case "Pyramid":
+                    {
+                        Shape2[0] = 'T';
+                        Shape2[1] = 'T';
+                        break;
+                    }
+            }
+            switch (three)
+            {
+                case "Cone":
+                    {
+                        Shape3[0] = 'T';
+                        Shape3[1] = 'C';
+                        break;
+                    }
+                case "Cube":
+                    {
+                        Shape3[0] = 'S';
+                        Shape3[1] = 'S';
+                        break;
+                    }
+                case "Sphere":
+                    {
+                        Shape3[0] = 'C';
+                        Shape3[1] = 'C';
+                        break;
+                    }
+                case "Prism":
+                    {
+                        Shape3[0] = 'S';
+                        Shape3[1] = 'T';
+                        break;
+                    }
+                case "Cylinder":
+                    {
+                        Shape3[0] = 'C';
+                        Shape3[1] = 'S';
+                        break;
+                    }
+                case "Pyramid":
+                    {
+                        Shape3[0] = 'T';
+                        Shape3[1] = 'T';
+                        break;
+                    }
+            }
+
+            string result = "";
+
+            //Check for Double Shapes
+            bool double1 = Shape1[0] == Shape1[1];
+            bool double2 = Shape2[0] == Shape2[1];
+            bool double3 = Shape3[0] == Shape3[1];
+            bool antiColumn1 = double1 && (AntiShape1 == Shape1[0]);
+            bool antiColumn2 = double2 && (AntiShape2 == Shape2[0]);
+            bool antiColumn3 = double3 && (AntiShape3 == Shape3[0]);
+            if (double1 && double2)
+            {
+                if (antiColumn1 && antiColumn2) //All Full anticolumns
+                {
+                    //Distrubute 1
+                    result += "Dissect " + charToString(AntiShape1) + " into LEFT\n";
+                    result += "Dissect " + charToString(AntiShape2) + " into MIDDLE\n";
+                    result += "Dissect " + charToString(AntiShape1) + " into LEFT\n";
+                    result += "Dissect " + charToString(AntiShape3) + " into RIGHT\n";
+
+                    //Quick Swap
+                    result += "Dissect " + charToString(AntiShape2) + " into MIDDLE\n";
+                    result += "Dissect " + charToString(AntiShape3) + " into RIGHT\n";
+                }
+                else if (antiColumn1)
+                {
+                    //Distribute 1
+                    result += "Dissect " + charToString(AntiShape1) + " into LEFT\n";
+                    result += "Dissect " + charToString(AntiShape2) + " into MIDDLE\n";
+                    result += "Dissect " + charToString(AntiShape1) + " into LEFT\n";
+                    result += "Dissect " + charToString(AntiShape3) + " into RIGHT\n";
+                }
+                else if (antiColumn2)
+                {
+                    //Distribute 2
+                    result += "Dissect " + charToString(AntiShape2) + " into MIDDLE\n";
+                    result += "Dissect " + charToString(AntiShape1) + " into LEFT\n";
+                    result += "Dissect " + charToString(AntiShape2) + " into MIDDLE\n";
+                    result += "Dissect " + charToString(AntiShape3) + " into RIGHT\n";
+                }
+                else if (antiColumn3)
+                {
+                    //Distribute 3
+                    result += "Dissect " + charToString(AntiShape3) + " into RIGHT\n";
+                    result += "Dissect " + charToString(AntiShape1) + " into LEFT\n";
+                    result += "Dissect " + charToString(AntiShape3) + " into RIGHT";
+                    result += "Dissect " + charToString(AntiShape2) + " into MIDDLE\n";
+                }
+                else //No Anti columns
+                {
+                    //Rotate
+                    result += "Dissect " + charToString(Shape1[0]) + " into LEFT\n";
+                    result += "Dissect " + charToString(Shape2[0]) + " into MIDDLE\n";
+                    result += "Dissect " + charToString(Shape2[0]) + " into MIDDLE\n";
+                    result += "Dissect " + charToString(Shape3[0]) + " into RIGHT\n";
+                }
+            }
+            else if(double1 || double2 || double3)//One double
+            {
+                if(antiColumn1)
+                {
+                    //Complex swap
+                    result += "Dissect " + charToString(AntiShape1) + " into LEFT\n";
+                    result += "Dissect " + charToString(AntiShape2) + " into MIDDLE\n";
+                    result += "Dissect " + charToString(AntiShape1) + " into LEFT\n";
+                    result += "Dissect " + charToString(AntiShape3) + " into RIGHT\n";
+                }
+                else if (antiColumn2)
+                {
+                    //Complex Swap
+                    result += "Dissect " + charToString(AntiShape2) + " into MIDDLE\n";
+                    result += "Dissect " + charToString(AntiShape1) + " into LEFT\n";
+                    result += "Dissect " + charToString(AntiShape2) + " into MIDDLE\n";
+                    result += "Dissect " + charToString(AntiShape3) + " into RIGHT\n";
+                }
+                else if (antiColumn3)
+                {
+                    result += "Dissect " + charToString(AntiShape3) + " into RIGHT\n";
+                    result += "Dissect " + charToString(AntiShape2) + " into MIDDLE\n";
+                    result += "Dissect " + charToString(AntiShape3) + " into RIGHT\n";
+                    result += "Dissect " + charToString(AntiShape1) + " into LEFT\n";
+                }
+                else
+                {
+                    //Quick Swap
+                    if(double1)
+                    {
+                        result += "Dissect " + charToString(Shape1[0]) + " into LEFT\n";
+                        if (Shape1[0] == AntiShape2)
+                            result += "Dissect " + charToString(Shape3[0]) + " into RIGHT\n";
+                        else
+                            result += "Dissect " + charToString(Shape2[0]) + " into MIDDLE\n";
+                    }
+                    if (double2)
+                    {
+                        result += "Dissect " + charToString(Shape2[0]) + " into MIDDLE\n";
+                        if (Shape1[0] == AntiShape1)
+                            result += "Dissect " + charToString(Shape3[0]) + " into RIGHT\n";
+                        else
+                            result += "Dissect " + charToString(Shape1[0]) + " into LEFT\n";
+                    }
+                    if (double3)
+                    {
+                        result += "Dissect " + charToString(Shape3[0]) + " into RIGHT\n";
+                        if (Shape1[0] == AntiShape2)
+                            result += "Dissect " + charToString(Shape1[0]) + " into LEFT\n";
+                        else
+                            result += "Dissect " + charToString(Shape2[0]) + " into MIDDLE\n";
+                    }
+                }
+            }
+            else//No Doubles
+            {
+                bool swapCol1 = (AntiShape1 == Shape1[0] || AntiShape1 == Shape1[1]);
+                bool swapCol2 = (AntiShape2 == Shape2[0] || AntiShape2 == Shape2[1]);
+                bool swapCol3 = (AntiShape3 == Shape3[0] || AntiShape3 == Shape3[1]);
+                if (swapCol1 && swapCol2)
+                {
+                    result += "Dissect " + charToString(AntiShape1) + " into LEFT\n";
+                    result += "Dissect " + charToString(AntiShape2) + " into MIDDLE\n";
+                }
+                else if (swapCol2 && swapCol3)
+                {
+                    result += "Dissect " + charToString(AntiShape2) + " into MIDDLE\n";
+                    result += "Dissect " + charToString(AntiShape3) + " into RIGHT\n";
+                }
+                else if (swapCol1 && swapCol3)
+                {
+                    result += "Dissect " + charToString(AntiShape1) + " into LEFT\n";
+                    result += "Dissect " + charToString(AntiShape3) + " into RIGHT\n";
+                }
+                else
+                {
+                    result += "Imagine Swapping this round\n";
+                }
+            }
+
+            ResultLabel.Text = result;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Solve();
+        }
+
+        private string charToString(char c)
+        {
+            if (c == 'C')
+            {
+                return "CIRCLE";
+            }
+            else if (c == 'T')
+            {
+                return "TRIANGLE";
+            }
+            else
+            {
+                return "SQUARE";
+            }
+        }
+    }
+}
